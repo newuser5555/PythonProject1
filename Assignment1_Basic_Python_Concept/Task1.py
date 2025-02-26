7c2a82349ea5be03b4ab8c8251cd23294d35d72a
sauru <sauru1234@com.com>
2025-02-26 15:30:33 +0530

    adding correct file

---
+++ Assignment1_Basic_Python_Concept/Task1.py
@@ -0,0 +1,16 @@
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+first_number = int(first_number)
+second_number = int(second_number)
+
+Addition = first_number + second_number
+print("Addition",Addition)
+
+Substraction = first_number - second_number
+print("Substraction", Substraction)
+
+Multiplication = first_number * second_number
+print("Multiplication", Multiplication)
+
+Division = first_number / second_number
+print("Division", Division)
