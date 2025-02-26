497e6580a541a6333b15c7a13778b64791cee95f
sauru <sauru1234@com.com>
2025-02-26 15:01:07 +0530

    Adding Task2

---
+++ Assignment/Task2.py
@@ -0,0 +1,7 @@
+
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+
+Full_name = First_name +" "+ Last_name
+print("Hello, " + Full_name + "! Welcome to the Python program.")
+
